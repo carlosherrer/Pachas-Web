dea1e6540699f59a0e64f4347ed895c7ee25b22a
Carlos Herrera <carlos.herrera.c@tecsup.edu.pe>
2024-05-25 13:35:44 -0500

    update carrousel :beer:

--- src/components/carrousel.jsx
+++ src/components/carrousel.jsx
@@ -4,34 +4,34 @@ import Mixto from "../assets/card/mixto.jpg";
 const carrousel = () => {
   return (
     <div>
-      <Carousel className="rounded-xl h-[540px]">
+      <Carousel className="rounded-xl h-[500px]">
         <div className="relative h-full w-full">
           <img
             src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
             alt="image 1"
-            className="h-[540px] w-full object-cover"
+            className="h-[500px] w-full object-cover"
           />
           <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
-            <div className="mt-24">
+            <div className="mt-16">
               <Typography
                 variant="h1"
                 color="white"
-                className="text-3xl md:text-4xl lg:text-5xl"
+                className="text-3xl text-center md:text-4xl lg:text-5xl"
               >
-                1er comentario
+                Opiniones de nuestros comensales
               </Typography>
             </div>
             <div className="flex justify-center gap-2 md:gap-10">
               <div>
-                <img src={Mixto} className="rounded-full w-40 md:w-64 md:mt-0 mt-4" />
+                <img src={Mixto} className="rounded-full w-36 md:w-64 md:mt-0 mt-4" />
               </div>
-              <div className="text-center w-3/5 md:w-3/12">
+              <div className="text-center w-1/3 md:w-3/12">
                 <Typography
                   variant="lead"
                   color="white"
-                  className="opacity-80 mb-20"
+                  className="opacity-80 md:mb-32 mb-40 text-sm md:text-2xl md:mt-8"
                 >
-                  &rdquo;Deliciosas parrillas con cortes jugosos y llenos de sabor. Ambiente acogedor y excelente servicio. ¡Muy recomendado!&rdquo;
+                  &ldquo;Deliciosas parrillas con cortes jugosos y llenos de sabor. Ambiente acogedor y excelente servicio. ¡Muy recomendado!&rdquo;
                 </Typography>
               </div>
             </div>
@@ -41,7 +41,7 @@ const carrousel = () => {
           <img
             src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
             alt="image 2"
-            className="h-[540px] w-full object-cover"
+            className="h-[500px] w-full object-cover"
           />
           <div className="absolute inset-0 grid h-full w-full items-center bg-black/75">
             <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
@@ -77,7 +77,7 @@ const carrousel = () => {
           <img
             src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
             alt="image 3"
-            className="h-[540px] w-full object-cover"
+            className="h-[500px] w-full object-cover"
           />
           <div className="absolute inset-0 grid h-full w-full items-end bg-black/75">
             <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
